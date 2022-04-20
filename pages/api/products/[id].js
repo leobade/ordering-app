6c85e714260f09689c286de11aa023f0a998dfd6
Leonardo Badellino <l.badellino@e-motion.com>
2022-04-20 17:57:30 +0200

    added admin page

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -22,8 +22,8 @@ export default async function handler(req, res) {
     }
     if (method === 'DELETE') {
         try {
-            const product = await Products.create(req.body)
-            res.status(201).json(product)
+            const product = await Products.findByIdAndDelete(id)
+            res.status(201).json('The product as been deleted')
         } catch (err) {
             res.status(400).json(err)
         }
